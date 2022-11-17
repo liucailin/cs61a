4006f5e75598c293823d0b3dadb28a1774d40e3d
liu <liu@work.com>
2022-11-17 16:45:44 +0800

    lab10 done

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,17 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond
+    ((= num1 num2) 0)
+    ((< num1 num2) -1)
+    ((> num1 num2) 1)
+))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (x) (+ num x)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define lst 'YOUR-CODE-HERE)
+(define lst '((1) 2 (3 4) 5))
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst)
+   (if (null? lst)
+       lst
+       (cons (car lst) (cons (car lst) (duplicate (cdr lst)))))
+)
