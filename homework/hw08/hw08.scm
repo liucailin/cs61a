98f89a0c771f176b9273ac3690e4d5946e82ce5f
liu <liu@work.com>
2022-11-17 19:25:54 +0800

    hw08 done

--- homework/hw08/hw08.scm
+++ homework/hw08/hw08.scm
@@ -1,8 +1,31 @@
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+  (cond
+    ((null? s) s)
+    ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+    (else (my-filter pred (cdr s)))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))
+  )
+)
 
 (define (accumulate joiner start n term)
-  'YOUR-CODE-HERE)
+  (if (= n 0)
+    start
+    (joiner (term n) (accumulate joiner start (- n 1) term))
+  )
+)
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+
+  (if (null? lst)
+    lst
+    (cons (car lst) (my-filter (lambda (x) (not (= x (car lst)))) (no-repeats (cdr lst))))
+  )
+    
+)
